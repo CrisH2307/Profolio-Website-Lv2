1d8a56e2fd07d9f2dcc2e31aae4cf851ffdae95b
Crisnineball <119613966+CrisH2307@users.noreply.github.com>
2023-08-10 20:46:52 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,72 @@
+/*===================typed====================*/
+var typed = new Typed(".text" , 
+{
+    strings : ["Frontend Developer",
+               "Backend Developer",
+               "UX Designer",
+               "Data Scientist as Software Analysts",
+               "Full Stack Developer"],
+               typeSpeed: 100,
+               backSpeed: 100,
+               backDelay: 1000,
+               loop: true
+});
+
+
+/*===================toggle icon navbar====================*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () =>
+{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+
+/*===================scroll sctions active link====================*/
+
+let sections = document.querySelectorAll('section');
+let navLink = document.querySelectorAll('header nav a');
+
+window.onscroll = () =>
+{
+    sections.forEach(sec =>
+    {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height)
+        {
+            navLink.forEach(links => 
+            {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+    /*===================Navbar====================*/
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    /*===================remove toggle icon and navbar when click navbar link(scroll)====================*/
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+};
+
+/*===================scroll reveal====================*/
+ScrollReveal({ 
+    //reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top'});
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom'});
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left'});
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right'});
